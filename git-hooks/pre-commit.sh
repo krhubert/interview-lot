ba266c3631977c6a5438c67e928c20e93282e65b
krhubert <krhubert@gmail.com>
2018-12-10 10:13:39 +0100

    Update git hooks

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-gometalinter         \
-  --vendor           \
-  --disable-all      \
-  --enable=gofmt     \
-  --enable=golint    \
-  --enable=goimports \
-  --enable=vetshadow \
-  --enable=misspell  \
-  --enable=vet       \
-  --min-confidence=1 ./...
+golangci-lint run
 
 exit $?
